b5ca4bb90c09ab10e6d0540ceadec254e90a417e
Tyler Thompson <studiesweekly@Tylers-MacBook-Pro.local>
2020-10-28 20:29:16 -0600

    changes to game.js

--- game.js
+++ game.js
@@ -1,11 +1,42 @@
 const buttonColors = ["red", "blue", "green", "yellow"];
 
-let gamePattern = []
+let gamePattern = [];
+
+let userClickedPattern = [];
+
+$(".btn").click(function(){
+  let userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+
+  playSound(userChosenColor);
+});
+
+
+
+
+
+
 
 let nextSequence = () => {
 
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColor = buttonColors[randomNumber]
   gamePattern.push(randomChosenColor)
-  console.log(gamePattern)
+
+  $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  playSound(randomChosenColor);
+
+
+}
+
+
+function playSound(name){
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+
+function animatePress(currentColor){
+  
 }
